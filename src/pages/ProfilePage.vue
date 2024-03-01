9ca75437dc5947b8e3be02ea8706869fdf06b042
Briden Dafoe <dafoebriden@gmail.com>
2024-03-01 00:12:42 -0700

    cant figure out modal

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,25 +1,27 @@
-<template>
-    <div class="container">
-        <section v-if="blog" class="row">
-            <div class="col-12">
-                <img src="" alt="">
-            </div>
-        </section>
-        <section v-for="blog in blogs" class="row blog">
-            <div class="col-8">
-                <div class="d-flex align-items-center mt-2">
-                    <button @click="setActiveBlog()" class="btn btn-primary" role="button" data-bs-toggle="modal"
-                        data-bs-target="#BlogModal">Open</button>
+<template v-if="profile">
+    <div>
+        <div class="container-fluid m-0 p-0">
+            <section class="row p-0 m-0">
+                <div class=" col-12 d-flex align-items-center justify-content-center mt-2">
+                    <img class="profile-img rounded-circle me-3" :src="profile.picture" :alt="profile.name">
+                    <h1>{{ profile.name }}</h1>
                 </div>
-                <div>
-                    <p class=" fw-bold m-0 px-3 pt-2">{{ blog.title }}</p>
-                    <p class="blog-body px-2 m-0 ">{{ blog.body }}</p>
+            </section>
+        </div>
+        <div class="container">
+            <section v-for="blog in blogs" class="row blog m-2">
+                <div class="col-8 height">
+                    <div class="selectable mt-2 rounded" @click="setActiveBlog()" role="button" data-bs-toggle="modal"
+                        data-bs-target="#BlogModal">
+                        <p class=" fw-bold m-0 px-3 pt-2">{{ blog.title }}</p>
+                        <p class="blog-body px-2 m-0 ">{{ blog.body }}</p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-4 p-0">
-                <img class="img-fluid post-img" :src="blog.imgUrl" :alt="blog.title">
-            </div>
-        </section>
+                <div class="col-4 p-0">
+                    <img class="img-fluid post-img" :src="blog.imgUrl" :alt="blog.title">
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 
@@ -72,12 +74,35 @@ export default {
 .post-img {
     height: 20vh;
     width: 100%;
+    border-top-right-radius: 2.2em;
+    border-bottom-right-radius: 2.2em;
+    margin-left: 1px;
+}
+
+.cover-img {
+    object-fit: contain;
+    object-position: center;
+}
+
+.profile-img {
+    height: 15vh;
+    width: 15vh;
+
 }
 
 .blog-body {
-    height: 15vh;
+    height: 10vh;
     width: fit-content;
     overflow: hidden;
+}
+
+.height {
+    height: 20vh;
+}
+
+.blog {
+    border: 10px solid black;
+    border-radius: 3em;
 
 }
 </style>
